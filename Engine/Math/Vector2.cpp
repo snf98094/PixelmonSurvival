ba34678063944692484e3e15ccc395a4dda072df
DESKTOP-JAGI02E\Minyeong <os5254@naver.com>
2025-02-25 11:12:28 +0900

    Vector2 길이 구하기, 방향 구하기 함수 추가.

--- Engine/Math/Vector2.cpp
+++ Engine/Math/Vector2.cpp
@@ -1,10 +1,70 @@
 #include "PreCompiledHeader.h"
 #include "Vector2.h"
 #include <cassert>
+#include <cmath>
+
+const Vector2 Zero = Vector2(0.0f);
+const Vector2 One = Vector2(1.0f);
+const Vector2 Right = Vector2(1.0f, 0.0f);
+const Vector2 Left = Vector2(-1.0f, 0.0f);
+const Vector2 Up = Vector2(0.0f, 1.0f);
+const Vector2 Down = Vector2(0.0f, -1.0f);
+
+Vector2::Vector2()
+    : x(0.0f), y(0.0f)
+{ }
+
+Vector2::Vector2(float value)
+    : x(value), y(value)
+{ }
 
 Vector2::Vector2(float x, float y)
     : x(x), y(y)
+{ }
+
+Vector2& Vector2::operator+=(const Vector2& other)
 {
+    x += other.x;
+    y += other.y;
+    return *this;
+}
+
+Vector2& Vector2::operator-=(const Vector2& other)
+{
+    x -= other.x;
+    y -= other.y;
+    return *this;
+}
+
+Vector2& Vector2::operator*=(float scale)
+{
+    x *= scale;
+    y *= scale;
+    return *this;
+}
+
+Vector2& Vector2::operator/=(float scale)
+{
+    assert(scale != 0.0f);
+
+    x /= scale;
+    y /= scale;
+    return *this;
+}
+
+bool Vector2::operator==(const Vector2& other)
+{
+    return x == other.x && y == other.y;
+}
+
+bool Vector2::operator!=(const Vector2& other)
+{
+    return x != other.x || y != other.y;
+}
+
+Vector2 Vector2::operator-() const
+{
+    return Vector2(-x, -y);
 }
 
 Vector2 Vector2::operator+(const Vector2& other)
@@ -28,12 +88,13 @@ Vector2 Vector2::operator/(const float scale)
     return Vector2(x / scale, y / scale);
 }
 
-bool Vector2::operator==(const Vector2& other)
+float Vector2::Length()
 {
-    return x == other.x && y == other.y;
+    return sqrtf(x * x + y * y);
 }
 
-bool Vector2::operator!=(const Vector2& other)
+Vector2 Vector2::Normalized()
 {
-    return x != other.x || y != other.y;
+    float length = Length();
+    return Vector2(x / length, y / length);
 }
--- Includes/Math/Vector2.h
+++ Includes/Math/Vector2.h
@@ -4,19 +4,42 @@
 class ENGINE_API Vector2
 {
 public:
-	Vector2(float x = 0, float y = 0);
+	Vector2();
+	Vector2(float value);
+	Vector2(float x, float y);
 	~Vector2() = default;
 
 	// 연산자 오버로딩.
+	Vector2& operator+=(const Vector2& other);
+	Vector2& operator-=(const Vector2& other);
+	Vector2& operator*=(float scale);
+	Vector2& operator/=(float scale);
+
+	bool operator==(const Vector2& other);
+	bool operator!=(const Vector2& other);
+
+	Vector2 operator-() const;
+
 	Vector2 operator+(const Vector2& other);
 	Vector2 operator-(const Vector2& other);
 	Vector2 operator*(const float scale);
 	Vector2 operator/(const float scale);
-	bool operator==(const Vector2& other);
-	bool operator!=(const Vector2& other);
+
+	// 길이 구하기.
+	float Length();
+	// 방향 구하기.
+	Vector2 Normalized();
 
 public:
 	// x좌표 / y좌표.
-	float x = 0;
-	float y = 0;
+	float x = 0.0f;
+	float y = 0.0f;
+
+	// 기본 값.
+	static const Vector2 Zero;
+	static const Vector2 One;
+	static const Vector2 Right;
+	static const Vector2 Left;
+	static const Vector2 Up;
+	static const Vector2 Down;
 };
